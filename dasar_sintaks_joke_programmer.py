923c631cfb8fd0deaf667cff4b3a52456f3be480
redhat <redhanoormansyah30@gmail.com>
2022-07-20 15:13:24 +0700

    Sequnsial & brancing

---
+++ dasar_sintaks_joke_programmer.py
@@ -0,0 +1,21 @@
+"""
+#Sekuensial
+"""
+print('ibu Berkata, "Pergi Ke toko"')
+print('Budi Menjawab, "baik, Apa yang harus dilakukan ditoko"')
+print('ibu Menjawab, "Beli, Satu Botol Susu, dan jika ada Telor beli 6"')
+print('Maka Budi Berangkat ke toko')
+print('dan Mulai berbelanja')
+
+# Percabangan
+
+jumlah_botol_susu = 173
+if jumlah_botol_susu > 0:
+    print("budi Mengecek Uang, Ternyata uangnya Cukup")
+    print("budi Membeli 1 botol Susu")
+else:
+    print("budi tidak membeli susu")
+
+print("budi Pulang kerumah ")
+print("menyerahkan kepada ibu")
+
--- main.py
+++
@@ -1,8 +0,0 @@
-"""
-#Sekuensial
-"""
-print('ibu Berkata, "Pergi Ke toko"')
-print('Budi Menjawab, "baik, Apa yang harus dilakukan ditoko"')
-print('ibu Menjawab, "Beli, Satu Botol Susu, dan jika ada Telor beli 6"')
-print('Maka Budi Berangkat ke toko')
-print('dan Mulai berbelanja')
