a58e0c302cae153c6d14e2ef9943664a724d4aa4
redhat <redhanoormansyah30@gmail.com>
2022-07-21 13:41:44 +0700

    looping

---
+++ dasar_sintaks_perulangan.py
@@ -0,0 +1,6 @@
+jumlah_buku_yang_harus_dibaca = 100
+print("Ibu Menyuruh membaca")
+jumlah_yang_dibaca = 0
+print("Ibu Menyuruh 100 Buku")
+for jumlah_yang_dibaca in range(1, jumlah_buku_yang_harus_dibaca+1):
+    print(f"buku ke - {jumlah_yang_dibaca} sudah dibaca Gais")
